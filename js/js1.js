5d89eb85bea6f260037a38a4a442ce5ed94b9004
Jiajun Xu <37936974@qq.com>
2017-08-07 13:44:23 +0800

    images splits, layout readjusting

--- js/js1.js
+++ js/js1.js
@@ -2,15 +2,16 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $interval) {
     $scope.stationName = "Potong Pasir Stn";
     $scope.buses = [
-        {"name":"197", "time":"3 min"},
-        {"name":"225", "time":"3 min"},
-        {"name":"149", "time":"5 min"},
-        {"name":"126", "time":"7 min"}
+        {"name":"197", "time":"3"},
+        {"name":"225", "time":"3"},
+        {"name":"149", "time":"5"},
+        {"name":"126", "time":"7"},
+        {"name":"57", "time":"12"}
     ];
     $scope.time = new Date();
     $interval(function () {
         $scope.time = new Date();
-    }, 1000)
+    }, 1000);
     $scope.pm = 174;
     $scope.temp = 35;
 });
