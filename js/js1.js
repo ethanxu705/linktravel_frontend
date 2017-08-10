0039850165cf15b5a6373106c7bf738ed01318d7
Jiajun Xu <37936974@qq.com>
2017-08-10 17:05:18 +0800

    finish page2 and page3

--- js/js1.js
+++ js/js1.js
@@ -1,12 +1,12 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $interval) {
+app.controller('myCtrl', function ($scope, $interval, $sce) {
     $scope.stationName = "Potong Pasir Stn";
     $scope.buses = [
-        {"name":"197", "time":"3"},
-        {"name":"225", "time":"3"},
-        {"name":"149", "time":"5"},
-        {"name":"126", "time":"7"},
-        {"name":"57", "time":"12"}
+        {"name": "197", "time": 3},
+        {"name": "25", "time": 3},
+        {"name": "149", "time": 5},
+        {"name": "126", "time": 7},
+        {"name": "57", "time": 12}
     ];
     $scope.time = new Date();
     $interval(function () {
@@ -14,4 +14,19 @@ app.controller('myCtrl', function($scope, $interval) {
     }, 1000);
     $scope.pm = 174;
     $scope.temp = 35;
+    $scope.myURL = $sce.trustAsResourceUrl("http://www.linktravel.com:8090/sense/bus");
+    $scope.news = [
+        {
+            "time": 5,
+            "title": "High Court sets out new sentencing framework for NS defaulters",
+            "content": "In a written judgment, the court said that the length of sentences should be amplified for those who have defaulted for a longer period of time, to \"reflect the decline in a person's physical fitness with ...",
+            "imgUrl":"../img/news1.png"
+        },
+        {
+            "time": 5,
+            "title": "High Court sets out new sentencing framework for NS defaulters",
+            "content": "In a written judgment, the court said that the length of sentences should be amplified for those who have defaulted for a longer period of time, to \"reflect the decline in a person's physical fitness with ...",
+            "imgUrl":"../img/news2.png"
+        }
+    ]
 });
